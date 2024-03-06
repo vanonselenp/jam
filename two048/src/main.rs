2c9d1f536d474bfb68d0bf8afb290e6a3438c61d
Peter van Onselen <petervanonselen@economist.com>
2024-03-06 18:44:04 +0000

    main: initial commit

---
+++ two048/src/main.rs
@@ -0,0 +1,20 @@
+use bevy::prelude::*;
+
+
+fn main() {
+    App::new().add_plugins(DefaultPlugins.set(
+        WindowPlugin {
+            primary_window: Some(Window {
+                title: "2048".to_string(),
+                ..default()
+            }),
+            ..default()
+        }
+    ))
+    .add_systems(Startup, setup)
+    .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
