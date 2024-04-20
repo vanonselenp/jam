710794422e6bebd05700ac93f3e03e3a017b2748
Peter van Onselen <vanonselen.p@gmail.com>
2024-04-20 14:26:01 +0100

    main: not sure what is going on here

--- two048/src/main.rs
+++ two048/src/main.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 
+const TILE_SIZE: f32 = 40.0;
 
 fn main() {
     App::new().add_plugins(DefaultPlugins.set(
@@ -12,7 +13,31 @@ fn main() {
         }
     ))
     .add_systems(Startup, setup)
+    .add_systems(Startup, spawn_board)
     .run();
+}
+
+#[derive(Component)]
+struct Board {
+    size: u8,
+}
+
+fn spawn_board(mut commands: Commands) {
+    let board = Board { size: 4 };
+    let physical_board_size = f32::from(board.size) * TILE_SIZE;
+
+    commands.spawn(
+        SpriteBundle {
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(
+                    physical_board_size,
+                    physical_board_size
+                )),
+                ..default()
+            },
+            ..default()
+        }
+    ).insert(board);
 }
 
 fn setup(mut commands: Commands) {
