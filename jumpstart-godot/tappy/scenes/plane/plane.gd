277ef1e6c887573afb767fc28b4ade46456439cc
Peter van Onselen <vanonselen.p@gmail.com>
2024-04-20 14:26:01 +0100

    main: progress

---
+++ jumpstart-godot/tappy/scenes/plane/plane.gd
@@ -0,0 +1,35 @@
+extends CharacterBody2D
+
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+
+const GRAVITY: float = 1500.0
+const POWER: float = -500.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	fly(delta)
+	
+	move_and_slide()
+	
+	if is_on_floor():
+		animated_sprite_2d.stop()
+
+
+func fly(delta: float) -> void:
+	velocity.y += GRAVITY * delta
+	
+	if Input.is_action_just_pressed("fly"):
+		velocity.y = POWER
+		
+		
+func death(delta: float) -> void:
+	set_physics_process(false)
+	animated_sprite_2d.stop()
+	
