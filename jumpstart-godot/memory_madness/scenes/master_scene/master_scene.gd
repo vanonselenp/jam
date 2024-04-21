0c08180e601581c76ba72f80e9b24fc853aaa16b
Peter van Onselen <vanonselen.p@gmail.com>
2024-04-21 09:23:00 +0100

    main: progress

---
+++ jumpstart-godot/memory_madness/scenes/master_scene/master_scene.gd
@@ -0,0 +1,34 @@
+extends CanvasLayer
+
+
+@onready var sound = $Sound
+@onready var main_screen = $MainScreen
+@onready var game_screen = $GameScreen
+
+
+enum GAME_STATE {
+	GAME,
+	MAIN
+}
+
+
+func _ready():
+	on_game_exited_pressed()
+	SignalManager.on_game_exited_pressed.connect(on_game_exited_pressed)
+	SignalManager.on_level_selected.connect(on_level_selected)
+
+
+func show_game(state: GAME_STATE) -> void:
+	game_screen.visible = state == GAME_STATE.GAME
+	main_screen.visible = state == GAME_STATE.MAIN
+
+
+func on_game_exited_pressed() -> void:
+	show_game(GAME_STATE.MAIN)
+	SoundManager.play_sound(sound, SoundManager.SOUND_MAIN_MENU)
+
+
+func on_level_selected(level_num: int) -> void:
+	show_game(GAME_STATE.GAME)
+	SoundManager.play_sound(sound, SoundManager.SOUND_IN_GAME)
+	
---
+++ jumpstart-godot/memory_madness/singletons/signal_manager.gd
@@ -0,0 +1,4 @@
+extends Node
+
+signal on_level_selected(level_num: int)
+signal on_game_exited_pressed
